c65ef1cf72688d6c0f0204711166d0dc628df8e3
Mahesh <94962227+Mahimahesh27@users.noreply.github.com>
2022-05-17 10:12:10 +0000

    lists

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
+fname =input("Enter file name: ")
+fh = open(fname)
+lst = list()                       
+for line in fh:                    
+    word= line.rstrip().split()    
+    for element in word:          
+        if element in lst:       
+            continue               
+        else :                     
+            lst.append(element)    
+lst.sort()                      
+print (lst)                         
 
-filename = "dataset/romeo.txt"
